e42c700a5aabb7aa4b5efb948ec65398fe4605a5
Albert Ge <lbertge@gmail.com>
2025-04-29 23:37:38 -0500

    Add benchmark for kmeans

---
+++ examples/bench_kmeans.sh
@@ -0,0 +1,18 @@
+python3 examples/quickstart_multinode.py 1 > r1.txt
+python3 examples/quickstart_multinode_with_partitions.py 1 13 use_kmeans > p1.txt
+python3 examples/quickstart_multinode.py 2 > r2.txt
+python3 examples/quickstart_multinode_with_partitions.py 2 12 use_kmeans > p2.txt
+python3 examples/quickstart_multinode.py 3 > r3.txt
+python3 examples/quickstart_multinode_with_partitions.py 3 12 use_kmeans > p3.txt
+python3 examples/quickstart_multinode.py 4 > r4.txt
+python3 examples/quickstart_multinode_with_partitions.py 4 12 use_kmeans > p4.txt
+python3 examples/quickstart_multinode.py 5 > r5.txt
+python3 examples/quickstart_multinode_with_partitions.py 5 12 use_kmeans > p5.txt
+python3 examples/quickstart_multinode.py 6 > r6.txt
+python3 examples/quickstart_multinode_with_partitions.py 6 11 use_kmeans > p6.txt
+python3 examples/quickstart_multinode.py 7 > r7.txt
+python3 examples/quickstart_multinode_with_partitions.py 7 11 use_kmeans > p7.txt
+python3 examples/quickstart_multinode.py 8 > r8.txt
+python3 examples/quickstart_multinode_with_partitions.py 8 11 use_kmeans > p8.txt
+python3 examples/quickstart_multinode.py 9 > r9.txt
+python3 examples/quickstart_multinode_with_partitions.py 9 11 use_kmeans > p9.txt
