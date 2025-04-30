9b3cb64ec8cfa5d3c656da3904280a57af1a26de
Grayson Elias <glelias@wisc.edu>
2025-04-29 22:31:06 -0500

    Added sample benchmark script

---
+++ examples/bench.sh
@@ -0,0 +1,18 @@
+python3 examples/quickstart_multinode.py 1 > r1.txt
+python3 examples/quickstart_multinode_with_partitions.py 1 13 > p1.txt
+python3 examples/quickstart_multinode.py 2 > r2.txt
+python3 examples/quickstart_multinode_with_partitions.py 2 12 > p2.txt
+python3 examples/quickstart_multinode.py 3 > r3.txt
+python3 examples/quickstart_multinode_with_partitions.py 3 12 > p3.txt
+python3 examples/quickstart_multinode.py 4 > r4.txt
+python3 examples/quickstart_multinode_with_partitions.py 4 12 > p4.txt
+python3 examples/quickstart_multinode.py 5 > r5.txt
+python3 examples/quickstart_multinode_with_partitions.py 5 12 > p5.txt
+python3 examples/quickstart_multinode.py 6 > r6.txt
+python3 examples/quickstart_multinode_with_partitions.py 6 11 > p6.txt
+python3 examples/quickstart_multinode.py 7 > r7.txt
+python3 examples/quickstart_multinode_with_partitions.py 7 11 > p7.txt
+python3 examples/quickstart_multinode.py 8 > r8.txt
+python3 examples/quickstart_multinode_with_partitions.py 8 11 > p8.txt
+python3 examples/quickstart_multinode.py 9 > r9.txt
+python3 examples/quickstart_multinode_with_partitions.py 9 11 > p9.txt
